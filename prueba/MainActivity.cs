3f44be6ecd377d413cf051491382c760fd41c478
rmorillo <rmorillo@ACERINO>
2016-04-07 19:36:03 -0400

    Add project files.

---
+++ prueba/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace prueba
+{
+    [Activity(Label = "prueba", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        TextView lblmensaje;
+        EditText txtusuario;
+        EditText txtclave;
+        Button MyButton;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            lblmensaje = FindViewById<TextView>(Resource.Id.lblmensaje);
+            txtusuario = FindViewById<EditText>(Resource.Id.txtusuario);
+            txtclave = FindViewById<EditText>(Resource.Id.txtclave);
+            MyButton = FindViewById<Button>(Resource.Id.MyButton);
+
+        }
+    }
+}
+
